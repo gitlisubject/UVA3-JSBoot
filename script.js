085abaf7d9ff1beb14c2ae0309eb12232851104d
gitlisubject <hrlee1509@gmail.com>
2024-06-25 20:50:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('consultaForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Muchas gracias por enviar su consulta. Su consulta fue enviado con exito.');
+});
